36f7c7f9d383833c587f3b33f67f2afad35152ef
Juan Verdugo <juanverdugobc@gmail.com>
2020-11-19 18:57:09 -0700

    First commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+// Initializations
+const app = express()
+
+
+// Settings
+app.set('port', process.env.PORT || 4000)
+
+// Middlewares
+
+
+// Global Variables
+
+
+// Routes
+
+
+// Static File
+
+
+module.exports = app;
